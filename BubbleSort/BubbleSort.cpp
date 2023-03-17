f6492e76b2a19b4acf9d2674e9396060653db513
BriliyanLuthfiTriandri <briliyanlt@gmail.com>
2023-03-17 16:04:11 +0700

    prosedur untuk memasukan atau menginput pada array

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -1,9 +1,18 @@
-
 #include <iostream>
-using namespace
+using namespace std;
 
-int a[20]
-int n;
+int a[20];            // Deklarasi array a dengan ukuran 20
+int n;                // Deklarasi variabel n untuk menyimpan banyaknya elemen pada array 
 
+void input() {         // procedur untuk input 
+	while (true) {     // looping 
+		cout << "Masukan banyaknya elemen pada array : "; // output ke layar 
+		cin >> n;      // Input dari pengguna
+		if (n <= 20)   // Jika n kurang dari atau sama dengan 20
+			break;     // Keluar dari loop
+		else {         // jika n lebih dari 20
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";  // Output ke layar
+	}
+}
 
 
